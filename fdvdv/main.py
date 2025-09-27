8d48021042cab2860090b71bede0602ccb35cb2e
san117893 <Sadchikov0311@Gmail.com>
2025-09-27 18:26:56 +0400

    Add files via upload

---
+++ fdvdv/main.py
@@ -0,0 +1,7 @@
+import random
+elements = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+pass_length = int(input("Enter pass length: "))
+password = ""
+for i in range(pass_length):
+    password += random.choice(elements)
+print(password)
